0a52909cca592e857043507352de1137bb51b427
zhangaixia602 <zhangaixia602@163.com>
2022-03-03 11:50:23 +0800

    tianjiathreejs

--- src/view/BusinessScope/index.jsx
+++ src/view/BusinessScope/index.jsx
@@ -1,8 +1,48 @@
 import React from 'react';
-export default class BusinessScope extends React.Component{
-  render(){
+import * as Three from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+// import { OrbitControls } from 'three/examples/jsm/controls/orbitcontrols';
+export default class BusinessScope extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  componentDidCatch() {
+    this.initThree();
+    this.animate();
+  }
+  initThree = () => {
+    let threeLoader = document.getElementById("threeLoader");
+    let width = threeLoader.clientWidth;
+    let height = threeLoader.clientHeight;
+    this.renderer = new Three.WebGL1Renderer({ antialias: true });//创建渲染器对象
+    this.renderer.setSize(width, height);
+    threeLoader.appendChild(this.renderer.domElement);
+    //设置相机
+    this.camera = new Three.PerspectiveCamera(50, width / height, 1, 10000);
+    this.camera.position.set(0, 0, 400);
+    this.camera.lookAt(this.scene.position);//设置相机的方向(指向场景对象)
+    //设置场景
+    this.scene = new Three.Scene();
+    // 辅助坐标系  参数250表示坐标系大小，可以根据场景大小去设置
+    let axisHelper = new Three.AxisHelper(250);
+    this.scene.add(axisHelper);
+    //设置相机控件
+    // this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+
+    //加载3D模型
+    let loader = new GLTFLoader().setPath("");
+    loader.load("../../models/Bee.glb", (gltf) => {
+      this.scene.add(gltf.scene);
+      this.renderer.render(this.scene, this.camera);
+    });
+  }
+  animate = () => {
+    requestAnimationFrame(this.animate);
+    this.renderer.render(this.scene, this.camera);
+  }
+  render() {
     return (
-      <div>业务领域</div>
+      <div id="threeLoader" style={{ width: "100%", height: "20rem" }}></div>
     )
   }
 }
--- src/view/Case/index.jsx
+++ src/view/Case/index.jsx
@@ -1,8 +1,43 @@
 import React from 'react';
-export default class Case extends React.Component{
-  render(){
+import * as Three from 'three';
+import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
+export default class Case extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  componentDidMount() {
+    this.init();
+    this.animate();
+  }
+  init = () => {
+    let threeModel = document.getElementById("threeModel");
+    this.scene = new Three.Scene();
+    this.camera = new Three.PerspectiveCamera(
+      45,
+      threeModel.clientWidth / threeModel.clientHeight,
+      0.01,
+      1000
+    );
+    this.renderer = new Three.WebGL1Renderer({ antialias: true });
+    this.renderer.setSize(threeModel.clientWidth, threeModel.clientHeight);
+    const cubeGeometry = new Three.BoxGeometry(4, 4, 4);
+    let cubeMaterial = new Three.MeshBasicMaterial({ color: 0x0000ff, wireframe: true });
+    let cube = new Three.Mesh(cubeGeometry, cubeMaterial);
+    cube.position.set(-4, 3, 0);
+    this.scene.add(cube);
+    this.camera.position.set(-30, 40, 30);
+    this.camera.lookAt(this.scene.position);
+    threeModel.appendChild(this.renderer.domElement);
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);//创建控件对象
+    this.renderer.render(this.scene, this.camera);
+  }
+  animate = () => {
+    requestAnimationFrame(this.animate);
+    this.renderer.render(this.scene, this.camera);
+  }
+  render() {
     return (
-      <div>案例module</div>
+      <div id="threeModel" style={{ width: "100%", height: "10rem" }}></div>
     )
   }
 }

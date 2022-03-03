8bf0495d935d33744a937d6fb69eb3455b10bf74
zhangaixia602 <zhangaixia602@163.com>
2022-03-03 14:23:26 +0800

    添加three模型

--- src/view/BusinessScope/index.jsx
+++ src/view/BusinessScope/index.jsx
@@ -1,39 +1,42 @@
 import React from 'react';
 import * as Three from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-// import { OrbitControls } from 'three/examples/jsm/controls/orbitcontrols';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import modelUrl from "../../assets/models/Bee.glb";
 export default class BusinessScope extends React.Component {
   constructor(props) {
     super(props)
   }
-  componentDidCatch() {
+  componentDidMount() {
     this.initThree();
     this.animate();
   }
   initThree = () => {
+    //创建渲染器
     let threeLoader = document.getElementById("threeLoader");
     let width = threeLoader.clientWidth;
     let height = threeLoader.clientHeight;
-    this.renderer = new Three.WebGL1Renderer({ antialias: true });//创建渲染器对象
+    this.renderer = new Three.WebGL1Renderer({ antialias: true });
     this.renderer.setSize(width, height);
     threeLoader.appendChild(this.renderer.domElement);
-    //设置相机
-    this.camera = new Three.PerspectiveCamera(50, width / height, 1, 10000);
-    this.camera.position.set(0, 0, 400);
-    this.camera.lookAt(this.scene.position);//设置相机的方向(指向场景对象)
     //设置场景
     this.scene = new Three.Scene();
-    // 辅助坐标系  参数250表示坐标系大小，可以根据场景大小去设置
-    let axisHelper = new Three.AxisHelper(250);
-    this.scene.add(axisHelper);
+    this.scene.background = new Three.Color( 0xeeeeee );
+     //设置光源
+     let light = new Three.HemisphereLight( 0xbbbbff, 0x444422, 1.5 );
+     light.position.set( 0, 1, 0 );
+     this.scene.add(light);
+     //设置相机
+     this.camera = new Three.PerspectiveCamera(50, width/height, 1, 10000);
+     this.camera.position.set(0, 0, 400);
+     this.camera.lookAt(this.scene.position);//设置相机的方向(指向场景对象)
     //设置相机控件
-    // this.controls = new OrbitControls(this.camera, this.renderer.domElement);
-
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
     //加载3D模型
-    let loader = new GLTFLoader().setPath("");
-    loader.load("../../models/Bee.glb", (gltf) => {
+    let loader = new GLTFLoader();
+    loader.load(modelUrl, (gltf) => {
       this.scene.add(gltf.scene);
-      this.renderer.render(this.scene, this.camera);
+      this.scene.scale.set(15, 15, 15);
     });
   }
   animate = () => {
@@ -42,7 +45,7 @@ export default class BusinessScope extends React.Component {
   }
   render() {
     return (
-      <div id="threeLoader" style={{ width: "100%", height: "20rem" }}></div>
+      <div id="threeLoader" style={{ width: "100%", height: "30rem" }}></div>
     )
   }
 }
